1490624c4bfef236166e9a40c7ab688e39e7a739
camilo tapia <camilo.tapia@gmail.com>
2014-05-25 10:57:36 +0200

    add production config

---
+++ configs/production.js
@@ -0,0 +1,11 @@
+var secrets = require('./production-secrets');
+
+exports.server = {
+    port: 8888,
+    url: "http://help-me-impress.1001.io"
+};
+
+exports.google = secrets.google;
+
+exports.mongodb = 'mongodb://localhost/help-me-impress-development';
+
